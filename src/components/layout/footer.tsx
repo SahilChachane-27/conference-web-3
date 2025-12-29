efce1f4969d8894d282c14fbbfed16cee8a646fd
Divya Chachane <divyachachane06@gmail.com>
2025-12-29 12:22:11 +0000

    these boxes arent properly aligned (_for element div_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -53,7 +53,7 @@ export function Footer() {
             </div>
             
             {/* Column 2: Navigation Links */}
-            <div className="space-y-4 flex flex-col items-center pt-4">
+            <div className="space-y-4 flex flex-col items-center">
                  <h2 className="font-headline text-2xl font-bold">
                     Quick <span className="text-accent">Links</span>
                 </h2>
@@ -79,7 +79,7 @@ export function Footer() {
             </div>
 
             {/* Column 3: Social Links */}
-            <div className="space-y-4 flex flex-col items-center pt-4">
+            <div className="space-y-4 flex flex-col items-center">
                  <h2 className="font-headline text-2xl font-bold">
                     Follow <span className="text-accent">Us</span>
                 </h2>
