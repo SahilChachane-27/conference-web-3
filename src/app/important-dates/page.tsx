48f91f476233efe4fbb274197582adfdfa449e02
Divya Chachane <divyachachane06@gmail.com>
2026-02-08 18:56:31 +0000

    These Registration Fees must be visible in the schedule page

--- src/app/important-dates/page.tsx
+++ src/app/important-dates/page.tsx
@@ -1,16 +1,28 @@
+'use client';
 
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { Schedule } from "@/components/sections/schedule";
+import { Tickets } from "@/components/sections/tickets";
+import { FirebaseClientProvider } from "@/firebase";
 
-export default function ImportantDatesPage() {
+function ImportantDatesPageContent() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-grow pt-20">
         <Schedule />
+        <Tickets />
       </main>
       <Footer />
     </div>
+  );
+}
+
+export default function ImportantDatesPage() {
+  return (
+    <FirebaseClientProvider>
+      <ImportantDatesPageContent />
+    </FirebaseClientProvider>
   );
 }
