be1267fe4acbc1d75262ddd9aa2ffc7c6e646a1b
Divya Chachane <divyachachane06@gmail.com>
2026-02-10 16:18:58 +0000

    Failed to compile. ./src/components/ui/calendar.tsx:57:9 Type error: Obj

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -56,7 +57,7 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      } as any}
       {...props}
     />
   )
